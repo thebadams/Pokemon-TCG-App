95e87fbb19b45e90b65e8fa26b7333018b028747
Brian Adams <bpeteradams@gmail.com>
2021-05-11 22:05:37 -0400

    wrote a weakness model

---
+++ models/Weakness.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Weakness extends Model{}
+
+Weakness.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    value: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    modelName: 'weakness',
+    underscored: true,
+  },
+);
+
+module.exports = Weakness;
