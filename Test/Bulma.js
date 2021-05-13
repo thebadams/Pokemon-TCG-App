442fa3461687f778dcf553f677139ae9d864a8c8
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-05-13 18:45:35 -0400

    commented sources started from scratch for nav bar

--- Test/Bulma.js
+++
@@ -1,6 +0,0 @@
-var burgerIcon = document.querySelector('#burger');
-var navbarMenu = document.querySelector('#nav-links');
-
-burgerIcon.addEventListener("click", () => {
-    navbarMenu.classList.toggle('is-active');
-});
---
+++ Test/Materialize.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//   $(".sidenav").sidenav();
+// });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
