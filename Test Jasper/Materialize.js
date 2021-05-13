4fa1541f55f7320e7371e4cc2fed2311fd11d6b4
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-05-13 19:51:01 -0400

    re fractored code

--- Test Jasper/Materialize.js
+++ Test Jasper/Materialize.js
@@ -1,3 +1,4 @@
-$(document).ready(function () {
-  $(".sidenav").sidenav();
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
 });
