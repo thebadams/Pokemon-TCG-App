ada31a15944d8201c0cbb1c1a415c52eed1441f0
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-05-13 20:24:00 -0400

    reset with Leigh's input

--- Test Jasper/Materialize.js
+++ Test Jasper/Materialize.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
+  var instances = M.Sidenav.init(elems, window.options);
 });
--- Test/Materialize.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function () {
-//   $(".sidenav").sidenav();
-// });
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
-});
