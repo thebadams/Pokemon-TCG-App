6b80d7e95d12152e836edeaaaf67511411777d8b
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-05-14 09:53:08 -0400

    Updated git ignore to include node_modules, added literal to add port on console log

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
