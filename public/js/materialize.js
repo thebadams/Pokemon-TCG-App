68f0cb14257d530c1d79c8841b6b9199317ea600
TMats <takomats@hotmail.com>
2021-05-14 23:31:16 -0400

    added a search by and autocomplete function.

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -5,14 +5,33 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Carousel
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.carousel');
   var instances = M.Carousel.init(elems, document.options);
 });
 
 //Modal
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.modal');
   var instances = M.Modal.init(elems, document.options);
 });
 
+// Autocomplete search bar
+var pokeData = {
+  "Pikachu": null,
+  "Bulbasaur": null,
+  "Ivysaur": null,
+  "Charmander": null,
+  "Squirtle": null,
+};
+
+$(document).ready(function () {
+  $('input.autocomplete').autocomplete({
+    data: pokeData,
+    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+    onAutocomplete: function (val) {
+      // Callback function when value is autcompleted.
+    },
+    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+  });
+});
