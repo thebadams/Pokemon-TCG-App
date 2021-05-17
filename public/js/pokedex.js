035c863dd8a0f4705cb1637bb518daaf382ee9bf
Clifford Morin <cemorin21@gmail.com>
2021-05-16 22:34:24 -0400

    got it working

--- public/js/pokedex.js
+++ public/js/pokedex.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const searchForm = document.querySelector('#searchForm');
 const addBtns = document.querySelectorAll('.add-btn');
 console.log(addBtns);
@@ -10,21 +11,23 @@ const handleSearchForm = async (event) => {
 
 searchForm.addEventListener('submit', handleSearchForm);
 
-const addToCollection = async(event) => {
+const addToCollection = async (event) => {
   event.preventDefault();
   console.log(event.target);
   const api_id = event.target.dataset.api;
   const card_name = event.target.dataset.name;
-  const description = event.target.dataset.description;
-  const card_image =event.target.dataset.image;
+  const { description } = event.target.dataset;
+  const card_image = event.target.dataset.image;
   const user_id = event.target.dataset.user;
   const info = {
     api_id,
     card_name,
     description,
     card_image,
-    user_id: 1,
+    user_id,
   };
+  console.log(user_id);
+  console.log(info);
   try {
     const response = fetch('/api/cards/', {
       method: 'POST',
@@ -33,18 +36,18 @@ const addToCollection = async(event) => {
         'Content-Type': 'application/json',
       },
     });
-    if(response.ok) {
-      alert("Success!");
+    if (response.ok) {
+      alert('Success!');
     }
   } catch (error) {
     console.log(error);
   }
   // const response = await fetch('/api/cards/', {
   //   method: 'POST',
-    
-  // })
-}
 
-addBtns.forEach((el=> {
+  // })
+};
+
+addBtns.forEach(((el) => {
   el.addEventListener('click', addToCollection);
 }));
