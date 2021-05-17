0223eb6a8f8440a0b41808f92fd6e3f0cb762773
Brian Adams <bpeteradams@gmail.com>
2021-05-16 20:35:06 -0400

    added event listeners to add buttons to add a card to a user's collection

--- public/js/pokedex.js
+++ public/js/pokedex.js
@@ -1,5 +1,6 @@
 const searchForm = document.querySelector('#searchForm');
-
+const addBtns = document.querySelectorAll('.add-btn');
+console.log(addBtns);
 const handleSearchForm = async (event) => {
   event.preventDefault();
   const searchQuery = document.querySelector('#autocomplete-input').value.trim();
@@ -8,3 +9,42 @@ const handleSearchForm = async (event) => {
 };
 
 searchForm.addEventListener('submit', handleSearchForm);
+
+const addToCollection = async(event) => {
+  event.preventDefault();
+  console.log(event.target);
+  const api_id = event.target.dataset.api;
+  const card_name = event.target.dataset.name;
+  const description = event.target.dataset.description;
+  const card_image =event.target.dataset.image;
+  const user_id = event.target.dataset.user;
+  const info = {
+    api_id,
+    card_name,
+    description,
+    card_image,
+    user_id: 1,
+  };
+  try {
+    const response = fetch('/api/cards/', {
+      method: 'POST',
+      body: JSON.stringify(info),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if(response.ok) {
+      alert("Success!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  // const response = await fetch('/api/cards/', {
+  //   method: 'POST',
+    
+  // })
+}
+
+addBtns.forEach((el=> {
+  el.addEventListener('click', addToCollection);
+}));
