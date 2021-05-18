8c52d50afc0d7051a10e8bdb2202d16a90ea7f8b
Brian Adams <bpeteradams@gmail.com>
2021-05-18 14:36:12 -0400

    got trading function to work

--- public/js/trading.js
+++ public/js/trading.js
@@ -24,10 +24,42 @@ otherCards.forEach((card) => {
 
 const tradeHandler = async (event) => {
   event.preventDefault();
-  const tradingCards = document.querySelectorAll('.trade');
-  const wantCards = document.querySelectorAll('.want')
-  console.log('Trading Cards:', tradingCards)
-  console.log('Want Cards:', wantCards);
+  const tradingCard = document.querySelector('.trade');
+  const wantCard = document.querySelector('.want');
+  console.log('Trading Cards:', tradingCard)
+  console.log('Want Cards:', wantCard);
+  const user_id1 = wantCard.dataset.user
+  const info1 = { user_id: user_id1 }
+  const user_id2 = tradingCard.dataset.user
+  const info2 = { user_id: user_id2 }
+  try {
+    const tradeData = await fetch(`/api/cards/${tradingCard.dataset.id}`, {
+    method: 'PUT',
+    body: JSON.stringify(info1),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  
+  console.log(tradeData);
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    const wantData = await fetch(`/api/cards/${wantCard.dataset.id}`, {
+    method: 'PUT',
+    body: JSON.stringify(info2),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  console.log(wantData)
+  } catch (error) {
+    console.log(error)
+  }
+  
+  document.location.replace('/trading');
 };
 
 tradeBtn.addEventListener('click', tradeHandler);
